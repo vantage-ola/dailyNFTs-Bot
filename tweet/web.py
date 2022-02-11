285eb9eb373b7c51106c7e195883b8df9027d633
vantage-ola <vantageola4@gmail.com>
2022-02-11 18:00:53 +0100

    deploy to server

---
+++ tweet/web.py
@@ -0,0 +1,21 @@
+from flask import Flask
+import atexit
+from apscheduler.schedulers.background import BackgroundScheduler
+
+from send_data import tweet_data
+
+app = Flask(__name__)
+
+@app.route('/')
+def job():
+    tweet_data()
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(func=job, trigger="interval", day=1)
+scheduler.start()
+
+atexit.register(lambda: scheduler.shutdown())
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
